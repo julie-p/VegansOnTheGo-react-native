f682fecdf1944deea09786313ed3a49c999ab44b
Julie <paupertjulie@gmail.com>
2021-03-23 20:16:30 +0100

    setup features and screens folders

--- App.js
+++ App.js
@@ -1,35 +1,14 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
-import SearchBar from "./src/components/Searchbar";
+
+/* Screens */
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <SearchBar />
-        </View>
-        <View style={styles.list}>
-          <Text>Restaurants List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
