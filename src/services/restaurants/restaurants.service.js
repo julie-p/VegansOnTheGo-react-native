f411379af48113e5cfbf150659da65f78bb4d44b
Julie <paupertjulie@gmail.com>
2021-03-31 14:04:32 +0200

    fetch data

---
+++ src/services/restaurants/restaurants.service.js
@@ -0,0 +1,19 @@
+import { mocks } from "./mock";
+import camelize from "camelize";
+
+export const restaurantRequest = (location = "37.7749295,-122.4194155") => {
+  return new Promise((resolve, reject) => {
+    const mock = mocks[location];
+    if (!mock) {
+      reject("not found");
+    }
+    resolve(mock);
+  });
+};
+restaurantRequest()
+  .then((result) => {
+    console.log(camelize(result));
+  })
+  .catch((err) => {
+    console.log("error");
+  });
