af03e8ee24380266095a7658ffaeffa7977c9d6a
Julie <paupertjulie@gmail.com>
2021-03-31 14:18:00 +0200

    transform data request

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -10,9 +10,21 @@ export const restaurantRequest = (location = "37.7749295,-122.4194155") => {
     resolve(mock);
   });
 };
+
+const restaurantsTransform = ({ results = [] }) => {
+  const mappedResults = results.map((restaurant) => {
+    return {
+      ...restaurant,
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
+    };
+  });
+  return camelize(mappedResults);
+};
 restaurantRequest()
-  .then((result) => {
-    console.log(camelize(result));
+  .then(restaurantsTransform)
+  .then((transformedResponse) => {
+    console.log(transformedResponse);
   })
   .catch((err) => {
     console.log("error");
